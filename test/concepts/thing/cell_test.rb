fa6e38e66b47c2793b5dc784c0d928db51407003
Zahidul Haque <iisowrovii@gmail.com>
2016-09-02 00:44:23 +0600

    fix cell test

--- test/concepts/thing/cell_test.rb
+++ test/concepts/thing/cell_test.rb
@@ -3,17 +3,16 @@ class Thing < ActiveRecord::Base
   class CellTest < ::Cell::TestCase
     controller ThingsController
 
-    let(:rails) { Thing::Create.(thing: {name: 'Rails'}) }
-    let(:trb){ Thing::Create.(thing: {name: 'Trailblazer'}) }
-    subject {
-      binding.pry
-      concept('thing/cell', collection: [trb, rails], last: rails).model
-    }
+    let(:rails) { Thing::Create.call(thing: { name: 'Rails' }).model }
+    let(:trb) { Thing::Create.call(thing: { name: 'Trailblazer' }).model }
+    subject do
+      concept('thing/cell', collection: [trb, rails], last: rails).call
+    end
 
     it do
       subject.must_have_selector '.columns .header a', text: 'Rails'
-      subject.must_not_have_selector '.columns.end .header a', text: 'Rails'
-      subject.must_have_selector '.columns.end .header a', text: 'Trailblazer'
+      subject.must_have_selector '.columns.end .header a', text: 'Rails'
+      subject.wont_have_selector '.columns.end .header a', text: 'Trailblazer'
     end
   end
 end
